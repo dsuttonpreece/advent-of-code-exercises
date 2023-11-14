d8dc61d893c82b1ac9788062ca58282d64779f66
dsuttonpreece <danielsuttonp@gmail.com>
2023-11-14 16:53:27 +0000

    wip: setup simple child actor communication

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,123 @@
-import { createActor, createMachine } from "xstate";
+import { assign, createActor, createMachine, sendTo } from "xstate";
 
-import { LogicGate, logicGateMachine } from "./logic-gate-machine";
-
-const machine = createMachine({
+const binaryGateMachine = createMachine({
   tsTypes: {} as import("./index.typegen").Typegen0,
-  schema: {
-    context: {
-      gates: [],
-    } as { gates: LogicGate[] },
+  id: "binary-gate",
+  on: {
+    SIGNAL: {
+      actions: ({ event }) => {
+        console.log("received");
+      },
+    },
   },
-    id: "machine",
-    
+  // initial: "Waiting",
+  // context: {
+  //   left: undefined,
+  //   right: undefined,
+  //   operator: undefined,
+  // },
+  // states: {
+  //   Waiting: {
+  //     entry: [
+  //       assign({
+  //         left: ({ event }) => event.input.left,
+  //         right: ({ event }) => event.input.right,
+  //         operator: ({ event }) => event.input.operator,
+  //       }),
+  //     ],
+  //     on: {
+  //       SIGNAL: [
+  //         {
+  //           target: "Done",
+  //           guard: ({ context, event }) => {
+  //             const { left, right, operator } = context;
+
+  //             if (operator === undefined)
+  //               throw new Error("BinaryGate - init without operator");
+
+  //             if (left || right) return false;
+  //             return true;
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   },
+  //   Done: {
+  //     type: "final",
+  //   },
+  // },
 });
 
-const actor = createActor(logicGateMachine, {
-  input: {
-    type: "signal",
-    value: 1,
-    target: "a",
+const signalMachine = createMachine({
+  tsTypes: {} as import("./index.typegen").Typegen1,
+  id: "signal",
+  initial: "Waiting",
+  context: {
+    target: undefined,
+    value: undefined,
+  },
+  states: {
+    Waiting: {
+      entry: [
+        assign({
+          target: ({ event }) => event.input.target,
+          value: ({ event }) => event.input.value,
+        }),
+      ],
+      always: "Done",
+    },
+    Done: {
+      entry: [
+        sendTo(
+          ({ context, system }) => system.get(context.target || ""),
+          ({ context, self }) => ({
+            type: 'SIGNAL',
+            value: context.value,
+            origin: self,
+          })
+        ),
+      ],
+      type: "final",
+    },
   },
 });
+
+const parentMachine = createMachine({
+  tsTypes: {} as import("./index.typegen").Typegen2,
+  id: "parent",
+  context: {},
+  entry: [
+    assign({
+      gateRefs: ({ spawn }) => [
+        spawn("binaryGate", {
+          systemId: "a",
+          input: {
+            operator: "AND",
+          },
+        }),
+      ],
+      signalRefs: ({ spawn }) => [
+        spawn("signal", {
+          input: { target: "a", value: 0 },
+        }),
+        spawn("signal", {
+          input: { target: "a", value: 1 },
+        }),
+      ],
+    }),
+  ],
+}).provide({
+  actors: {
+    signal: signalMachine,
+    binaryGate: binaryGateMachine,
+  },
+});
+
+const actor = createActor(parentMachine);
 
 // Subscribe to updated snapshots (emitted state changes) from the actor
 actor.subscribe((snapshot: any) => {
-  console.log("Value:", snapshot.value);
+  // console.log("Value:", snapshot);
 });
 
 actor.start();
--- src/index.typegen.ts
+++ src/index.typegen.ts
@@ -30,4 +30,64 @@
         matchesStates: undefined;
         tags: never;
       }
+
+export interface Typegen1 {
+        '@@xstate/typegen': true;
+        internalEvents: {
+          "xstate.init": { type: "xstate.init" };
+        };
+        invokeSrcNameMap: {
+          
+        };
+        missingImplementations: {
+          actions: never;
+          delays: never;
+          guards: never;
+          services: never;
+        };
+        eventsCausingActions: {
+          
+        };
+        eventsCausingDelays: {
+          
+        };
+        eventsCausingGuards: {
+          
+        };
+        eventsCausingServices: {
+          
+        };
+        matchesStates: "Done" | "Waiting";
+        tags: never;
+      }
+
+export interface Typegen2 {
+        '@@xstate/typegen': true;
+        internalEvents: {
+          "xstate.init": { type: "xstate.init" };
+        };
+        invokeSrcNameMap: {
+          
+        };
+        missingImplementations: {
+          actions: never;
+          delays: never;
+          guards: never;
+          services: never;
+        };
+        eventsCausingActions: {
+          
+        };
+        eventsCausingDelays: {
+          
+        };
+        eventsCausingGuards: {
+          
+        };
+        eventsCausingServices: {
+          
+        };
+        matchesStates: undefined;
+        tags: never;
+      }
   
