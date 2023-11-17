c3e9390a6a9ccfd46e8560010ffc4cef540701b3
dsuttonpreece <danielsuttonp@gmail.com>
2023-11-17 14:24:11 +0000

    feat: generate machine spec from instructions

--- src/index.ts
+++ src/index.ts
@@ -15,14 +15,14 @@ NOT y -> i
 
 const operators = ["AND", "OR", "LSHIFT", "RSHIFT", "NOT"] as const;
 
-const buildSignalMachine = (initialContext: {
+const buildSignalInvocation = (initialContext: {
   wireOut: string;
   out: number;
 }) => {
   const { wireOut, out } = initialContext;
 
   return {
-    [`signal.${out}`]: {
+    [`signal_${out}`]: {
       invoke: {
         src: "signalMachine",
         input: {
@@ -34,9 +34,9 @@ const buildSignalMachine = (initialContext: {
   };
 };
 
-const buildWireMachine = (id) => {
+const buildWireInvocation = (id) => {
   return {
-    [`wire.${id}`]: {
+    [`wire_${id}`]: {
       invoke: {
         src: "wireMachine",
         systemId: id,
@@ -45,7 +45,7 @@ const buildWireMachine = (id) => {
   };
 };
 
-const buildGateMachine = (initialContext: {
+const buildGateInvocation = (initialContext: {
   wiresIn: string[];
   operator: (typeof operators)[number];
   wireOut: string;
@@ -63,14 +63,16 @@ const buildGateMachine = (initialContext: {
   }
 
   return {
-    [`gate.${[wiresIn[0], operator, wiresIn[1]].filter(Boolean).join(".")}`]: {
-      src: "gateMachine",
-      input: {
-        wiresIn,
-        in: valuesIn,
-        operator,
-        wireOut,
-      },
+    [`gate_${[wiresIn[0], operator, wiresIn[1]].filter(Boolean).join("_")}`]: {
+      invoke: {
+        src: "gateMachine",
+        input: {
+          wiresIn,
+          in: valuesIn,
+          operator,
+          wireOut,
+        },
+      }
     },
   };
 };
@@ -78,7 +80,7 @@ const buildGateMachine = (initialContext: {
 function parseInstructions(instructions: string) {
   const lines = instructions.split("\n").filter(Boolean);
 
-  const definitions = lines.reduce(
+  const invocationStates = lines.reduce(
     (acc, line): any => {
       const [gateDefinition, wireOut] = line.split(" -> ");
 
@@ -88,10 +90,10 @@ function parseInstructions(instructions: string) {
         case 1:
           return {
             ...acc,
-            wires: [...acc.wires, buildWireMachine(wireOut)],
+            wires: [...acc.wires, buildWireInvocation(wireOut)],
             signals: [
               ...acc.signals,
-              buildSignalMachine({
+              buildSignalInvocation({
                 wireOut,
                 out: parseInt(pieces[0]),
               }),
@@ -102,12 +104,12 @@ function parseInstructions(instructions: string) {
             ...acc,
             wires: [
               ...acc.wires,
-              buildWireMachine(pieces[1]),
-              buildWireMachine(wireOut),
+              buildWireInvocation(pieces[1]),
+              buildWireInvocation(wireOut),
             ],
             gates: [
               ...acc.gates,
-              buildGateMachine({
+              buildGateInvocation({
                 wiresIn: [pieces[1]],
                 operator: pieces[0] as (typeof operators)[number],
                 wireOut,
@@ -119,13 +121,13 @@ function parseInstructions(instructions: string) {
             ...acc,
             wires: [
               ...acc.wires,
-              buildWireMachine(pieces[0]),
-              buildWireMachine(pieces[2]),
-              buildWireMachine(wireOut),
+              buildWireInvocation(pieces[0]),
+              buildWireInvocation(pieces[2]),
+              buildWireInvocation(wireOut),
             ],
             gates: [
               ...acc.gates,
-              buildGateMachine({
+              buildGateInvocation({
                 wiresIn: [pieces[0], pieces[2]],
                 operator: pieces[1] as (typeof operators)[number],
                 wireOut,
@@ -137,107 +139,27 @@ function parseInstructions(instructions: string) {
     { signals: [], wires: [], gates: [] }
   );
 
-  return definitions;
+
+  
+  return {
+    id: "breadboard",
+    type: "parallel",
+    states: {
+      // Wires
+      ...Object.assign({}, ...invocationStates.wires),
+  
+      // Gates
+      ...Object.assign({}, ...invocationStates.gates),
+  
+      // Signals
+      ...Object.assign({}, ...invocationStates.signals)
+    },
+  }
 }
 
-console.log(parseInstructions(demoInstructions));
+const demoMachineSpec = parseInstructions(demoInstructions)
 
-const parentMachine = createMachine({
-  // @ts-ignore
-  tsTypes: {} as import("./index.typegen").Typegen0,
-  id: "parent",
-  context: {},
-  type: "parallel",
-  states: {
-    // Wires
-    wireX: {
-      invoke: {
-        src: "wireMachine",
-        systemId: "x",
-      },
-    },
-    wireY: {
-      invoke: {
-        src: "wireMachine",
-        systemId: "y",
-      },
-    },
-
-    // Gates
-    gateXandY: {
-      invoke: {
-        src: "gateMachine",
-        input: {
-          wiresIn: ["x", "y"],
-          operator: "AND",
-          wireOut: "d",
-        },
-      },
-    },
-    gateXorY: {
-      invoke: {
-        src: "gateMachine",
-        input: {
-          wiresIn: ["x", "y"],
-          operator: "OR",
-          wireOut: "e",
-        },
-      },
-    },
-    gateLShiftX2: {
-      invoke: {
-        src: "gateMachine",
-        input: {
-          wiresIn: ["x"],
-          in: [undefined, 2],
-          operator: "LSHIFT",
-          wireOut: "f",
-        },
-      },
-    },
-    gateRShiftY2: {
-      invoke: {
-        src: "gateMachine",
-        input: {
-          wiresIn: ["y"],
-          in: [undefined, 2],
-          operator: "RSHIFT",
-          wireOut: "g",
-        },
-      },
-    },
-    gateNotX: {
-      invoke: {
-        src: "gateMachine",
-        input: {
-          wiresIn: ["x"],
-          operator: "NOT",
-          wireOut: "h",
-        },
-      },
-    },
-
-    // Signals
-    signal123: {
-      invoke: {
-        src: "signalMachine",
-        input: {
-          wireOut: "x",
-          out: 123,
-        },
-      },
-    },
-    signal456: {
-      invoke: {
-        src: "signalMachine",
-        input: {
-          wireOut: "y",
-          out: 456,
-        },
-      },
-    },
-  },
-}).provide({
+const parentMachine = createMachine(demoMachineSpec).provide({
   actors: {
     gateMachine,
     wireMachine,
@@ -252,4 +174,4 @@ actor.subscribe((snapshot: any) => {
   // console.log("Value:", snapshot);
 });
 
-// actor.start();
+actor.start();
